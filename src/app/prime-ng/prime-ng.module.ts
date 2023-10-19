9fd0783dcb8be48afab10f880166352bc1408f91
Fernando Reyez Pinto <f.reyesft@gmail.com>
2023-10-18 19:32:09 -0500

    new values in to uncommon pages

--- src/app/prime-ng/prime-ng.module.ts
+++ src/app/prime-ng/prime-ng.module.ts
@@ -3,8 +3,8 @@ import { ButtonModule } from 'primeng/button';
 import { MenuModule } from 'primeng/menu'
 import { MenubarModule } from 'primeng/menubar';
 import { CardModule } from 'primeng/card'
-import { FieldsetModule } from 'primeng/fieldset'
 import { PanelModule } from 'primeng/panel'
+import { FieldsetModule } from 'primeng/fieldset';
 @NgModule({
   exports: [
     MenuModule,
--- src/app/products/pages/uncommon-page/uncommon-page.component.ts
+++ src/app/products/pages/uncommon-page/uncommon-page.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Observable, interval, tap } from 'rxjs';
 
 @Component({
   selector: 'app-uncommon-page',
@@ -6,5 +7,44 @@ import { Component } from '@angular/core';
   styleUrls: ['./uncommon-page.component.css']
 })
 export class UncommonPageComponent {
+  public name: string = "Fernando";
+  public gender: 'male' | 'female' = `male`;
+  public clients: string[] = [
+    'Maria', 'Pedro', 'Abuela', 'Brandon', 'Jonathan', 'Maicol', 'Juan'
+  ]
 
+  public clientsMap = {
+    '=0': 'no tenemos ningun cliente',
+    '=1': 'tenemos un cliente esperando',
+    'other': 'tenemos # clientes esperando'
+  }
+
+  public person = {
+    name: 'Fernado',
+    age: 19,
+    address: 'Colombia'
+  }
+
+  public myObservableTimer: Observable<number> = interval(1000).pipe(
+    tap(value => value)
+  );
+
+  public promiseValue: Promise<string> = new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve('JIJIJIJIJJIJIJIJIJI')
+    }, 1000);
+  })
+
+  public invitationMap = {
+    'male': 'invitarlo',
+    'female': 'invitarla'
+  }
+  changeClient(): void {
+    this.name = 'Maria'
+    this.gender = 'female'
+  }
+
+  deleteClient(): void {
+    this.clients.shift()
+  }
 }
